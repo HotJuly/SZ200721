4f1f365c0cd20d4e2894953244082d1f1a892d94
HotJuly <949588380@qq.com>
2021-01-11 10:57:09 +0800

    05.reactive+toRefs

---
+++ 4.Vue3/02.代码/vue3_test/src/views/01.ref使用.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    I'm Home
+    <h2>count: {{count}}</h2>
+    <button @click="handleClick">+1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent , ref } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    
+  },
+  // data(){
+  //   return {
+
+  //   }
+  // },
+  setup(){
+    // 注册在模版中需要使用到的响应式数据和修改响应式数据的函数
+    console.log('setup')
+
+    //ref函数,生成的是一个Ref对象,他身上有value属性,不过他是一个getter
+    // template中想使用响应式属性,直接写名字,不需要写.value
+    // 在js代码中,想使用响应式属性的值或者修改,都必须要.value
+    const count = ref(0);
+    console.log(count)
+
+    const handleClick = function(){
+      count.value++;
+    }
+
+    return {
+      count,
+      handleClick
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate')
+  },
+})
+</script>
---
+++ 4.Vue3/02.代码/vue3_test/src/views/02.computed.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    I'm Home
+    <h2>count: {{count}}</h2>
+    <h2>doubleCount: {{double}}</h2>
+    <h2>ThreeCount: {{three}}</h2>
+    <button @click="handleClick">+1</button>
+    <button @click="clickThree">three点击</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent , ref , computed} from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  setup(){
+    const count = ref<number>(0);
+
+    const handleClick = function(){
+      count.value++;
+    }
+
+    // Vue中计算属性本质是一个函数
+    const double = computed(()=>{
+      return count.value*2
+    });
+
+    
+    const three = computed<number>({
+      get () {
+        return count.value*3;
+      },
+      set (newVal) {
+        // console.log('set')
+        count.value=newVal;
+      }
+    });
+
+    const clickThree = function(){
+      three.value=1111;
+    }
+
+
+
+
+
+    return {
+      count,
+      handleClick,
+      double,
+      three,
+      clickThree
+    }
+  },
+  // computed:{
+  //   a(){
+  //     return 123;
+  //   }
+  // },
+})
+</script>
